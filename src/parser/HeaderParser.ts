3cb88f26027f63d0bee887b4da6694654b37dd38
Prayansh Srivastava <prayansh@gmail.com>
2019-10-07 13:37:23 -0700

    header parse v1

--- src/parser/HeaderParser.ts
+++ src/parser/HeaderParser.ts
@@ -1,8 +1,38 @@
 import {AbstractParser} from "./AbstractParser";
 import Tokenizer from "../tokenizer/Tokenizer";
+import ParserError from "./ParserError";
+import Tokens from "../tokenizer/Tokens";
+import {Header} from "../types/Header";
+import PlaintextParser from "./PlaintextParser";
+import {PlainText} from "../types/Text";
 
-export default class HeaderParser extends AbstractParser{
-    parse(context: Tokenizer): void {
+export default class HeaderParser extends AbstractParser {
+    parse(context: Tokenizer): Header {
+        // Get NAME
+        let headerStart = context.getNext()
+        if (headerStart.charAt(0) !== Tokens.USER_NAME) {
+            throw new ParserError(`Expected ${Tokens.USER_NAME} but got ${headerStart.charAt(0)} ` +
+                `at line: ${context.getCurrentLine()}`)
+        }
+        let nameParsed = new PlaintextParser(Tokens.NEW_LINE).parse(context);
+        context.getNext() // consume NEW_LINE
+
+        // Get Links
+        let links = [] as Array<PlainText>
+        while (context.peek() !== Tokens.HEADER_BODY_DIV) {
+            links.push(this.parseLink(context))
+        }
+
+        return {
+            name: nameParsed,
+            links: links
+        }
+    }
+
+    parseLink(context: Tokenizer): PlainText {
+        let linkParsed = new PlaintextParser(Tokens.NEW_LINE).parse(context);
+        context.getNext() // consume NEW_LINE
+        return linkParsed
     }
 
 }
