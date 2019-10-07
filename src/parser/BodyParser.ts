596ff89979d8b9f264324cc60ace63142eaf1d87
Prayansh Srivastava <prayansh@gmail.com>
2019-10-07 16:32:33 -0700

    add basic types

--- src/parser/BodyParser.ts
+++ src/parser/BodyParser.ts
@@ -1,12 +1,16 @@
 import {AbstractParser} from "./AbstractParser";
 import Tokenizer from "../tokenizer/Tokenizer";
 import SectionParser from "./SectionParser";
+import {Body} from "../types/Body"
+import {Section} from "../types/Section";
 
 export default class BodyParser extends AbstractParser {
-    parse(context: Tokenizer): void {
+    parse(context: Tokenizer): Body {
+        let body = [] as Array<Section>
         do {
             let section = new SectionParser();
-            section.parse(context)
-        } while(!context.hasNext())
+            body.push(section.parse(context))
+        } while (!context.hasNext())
+        return body
     }
 }
