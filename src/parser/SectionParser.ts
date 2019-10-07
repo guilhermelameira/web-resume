5a5cf3b27c40baa174ef69310ebab73999b6faad
Prayansh Srivastava <prayansh@gmail.com>
2019-10-07 16:32:33 -0700

    add basic types

--- src/parser/SectionParser.ts
+++ src/parser/SectionParser.ts
@@ -1,9 +1,31 @@
 import {AbstractParser} from "./AbstractParser";
 import Tokenizer from "../tokenizer/Tokenizer";
-import ParserError from "./ParserError";
+import Tokens from "../tokenizer/Tokens";
+import PlaintextParser from "./PlaintextParser";
+import {Entry, Section} from "../types/Section";
+import {EntryParser} from "./EntryParser";
 
 export default class SectionParser extends AbstractParser {
-    parse(context: Tokenizer): void {
-        throw new ParserError("NOT IMPLEMENTED")
+    parse(context: Tokenizer): Section {
+        // Look for Section.ts Title
+        let sectionTitleKey = context.getNext()
+        let sectionTitle = {
+            value: ""
+        }
+        if (sectionTitleKey !== Tokens.SECTION_TITLE_KEY) {
+            sectionTitle = new PlaintextParser(Tokens.NEW_LINE).parse(context);
+            context.getNext() // consume NEW_LINE
+        }
+        // Parse Entries
+        let entries = [] as Array<Entry>
+        while (context.peek() !== Tokens.SECTION_TITLE_KEY || !context.hasNext()) {
+            let entry = new EntryParser().parse(context);
+            entries.push(entry)
+        }
+
+        return {
+            title: sectionTitle,
+            entries: entries
+        }
     }
 }
