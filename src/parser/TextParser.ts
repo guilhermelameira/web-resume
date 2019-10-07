922e7f396e5bee791234cd2cd4143256536575ef
Prayansh Srivastava <prayansh@gmail.com>
2019-10-07 16:35:21 -0700

    stub for TextParser

---
+++ src/parser/TextParser.ts
@@ -0,0 +1,11 @@
+import {AbstractParser} from "./AbstractParser";
+import Tokenizer from "../tokenizer/Tokenizer";
+import {Text} from "../types/Text"
+
+export class TextParser extends AbstractParser {
+    parse(context: Tokenizer): Text {
+        // TODO implement
+        return {} as Text
+    }
+
+}
