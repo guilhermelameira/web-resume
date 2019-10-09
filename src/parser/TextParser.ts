107ef17e6ead5339a517e6807afe835eea088833
egeokt <egeokten@gmail.com>
2019-10-08 23:30:03 -0700

    Implement text parser

--- src/parser/TextParser.ts
+++ src/parser/TextParser.ts
@@ -1,11 +1,42 @@
 import {AbstractParser} from "./AbstractParser";
 import Tokenizer from "../tokenizer/Tokenizer";
-import {Text} from "../types/Text"
+import {Text} from "../types/Text";
+import Tokens from "../tokenizer/Tokens";
+import PlaintextParser from "./PlaintextParser";
 
 export class TextParser extends AbstractParser {
     parse(context: Tokenizer): Text {
-        // TODO implement
-        return {} as Text
+        
+        let text = {} as Text
+
+        // check next token and evaluate based on the type of the text
+        let nextToken = context.peek()
+        switch(nextToken){
+            case Tokens.EMPHASIS_DECORATOR_START:
+                // emphasis text
+                context.getNext()   // consume emphasis start token
+                let plaintextParser = new PlaintextParser(Tokens.EMPHASIS_DECORATOR_END)
+                let plainText = plaintextParser.parse(context)
+                text.value = plainText.value
+                text.decorator = 'EMPHASIS'
+                break;
+            case Tokens.TOKEN_DECORATOR_START:
+                // tokened text(skill)
+                context.getNext()  // consume token(skill) start token
+                let pTextParser = new PlaintextParser(Tokens.TOKEN_DECORATOR_END)
+                let pText = pTextParser.parse(context)
+                text.value = pText.value
+                text.decorator = 'TOKEN'
+                break;
+            default:
+                // raw text(normal string: no emphasis, no token)
+
+                // todo: maybe modify the plaintextparser?
+                break;
+                
+
+        }
+        return text
     }
 
 }
--- src/tokenizer/Tokens.ts
+++ src/tokenizer/Tokens.ts
@@ -1,6 +1,6 @@
 // List of Tokens (in regex) in our language
 export default class Tokens {
-    public static HEADER_BODY_DIV = "==="
+    public static HEADER_BODY_DIV = "=="
     public static EMPHASIS_DECORATOR_START = "{{"
     public static EMPHASIS_DECORATOR_END = "}}"
     public static NEW_LINE = "NEW_LINE"
@@ -8,5 +8,8 @@ export default class Tokens {
     public static SECTION_TITLE_KEY = "#"
     public static ENTRY_TITLE_KEY = "##"
     public static ENTRY_SUBTITLE_KEY = "###"
+    public static TOKEN_DECORATOR_START = "(("
+    public static TOKEN_DECORATOR_END = "))"
+    public static BULLET_KEY = "-"
 }
 
