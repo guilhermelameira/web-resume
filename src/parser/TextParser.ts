6dfca683a963c20f7280ae9532bb116faacdd883
egeokt <egeokten@gmail.com>
2019-10-10 20:23:00 -0700

    Implement text parser for 3 different types

--- src/parser/TextParser.ts
+++ src/parser/TextParser.ts
@@ -1,41 +1,47 @@
-import {AbstractParser} from "./AbstractParser";
+import { AbstractParser } from "./AbstractParser";
 import Tokenizer from "../tokenizer/Tokenizer";
-import {Text} from "../types/Text";
+import { Text, PlainText } from "../types/Text";
 import Tokens from "../tokenizer/Tokens";
 import PlaintextParser from "./PlaintextParser";
 
 export class TextParser extends AbstractParser {
     parse(context: Tokenizer): Text {
-        
+
         let text = {} as Text
+        let plainTextParser = {} as PlaintextParser
+        let initialText = {} as PlainText
+
 
         // check next token and evaluate based on the type of the text
         let nextToken = context.peek()
-        switch(nextToken){
+        switch (nextToken) {
             case Tokens.EMPHASIS_DECORATOR_START:
                 // emphasis text
-                context.getNext()   // consume emphasis start token
-                let plaintextParser = new PlaintextParser(Tokens.EMPHASIS_DECORATOR_END)
-                let plainText = plaintextParser.parse(context)
-                text.value = plainText.value
+                context.getNext()   // consume the emphasis start token
+                plainTextParser = new PlaintextParser(Tokens.EMPHASIS_DECORATOR_END)
+                initialText = plainTextParser.parse(context)
+                text.value = initialText.value
                 text.decorator = 'EMPHASIS'
+                context.getNext()   // consume the emphasis end token
                 break;
             case Tokens.TOKEN_DECORATOR_START:
                 // tokened text(skill)
-                context.getNext()  // consume token(skill) start token
-                let pTextParser = new PlaintextParser(Tokens.TOKEN_DECORATOR_END)
-                let pText = pTextParser.parse(context)
-                text.value = pText.value
+                context.getNext()  // consume the token(skill) start token
+                plainTextParser = new PlaintextParser(Tokens.TOKEN_DECORATOR_END)
+                initialText = plainTextParser.parse(context)
+                text.value = initialText.value
                 text.decorator = 'TOKEN'
+                context.getNext()   // consume the token end token
                 break;
             default:
                 // raw text(normal string: no emphasis, no token)
-
-                // todo: maybe modify the plaintextparser?
+                plainTextParser = new PlaintextParser("")
+                initialText = plainTextParser.parseTextInSentence(context)
+                text.value = initialText.value
+                text.decorator = 'RAW'
                 break;
-                
-
         }
+
         return text
     }
 
