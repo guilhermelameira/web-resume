484b105c8cbf5d490ec8f820272e304609551e2f
egeokt <egeokten@gmail.com>
2019-10-10 20:23:30 -0700

    Implement summary parser

--- src/parser/SummaryParser.ts
+++ src/parser/SummaryParser.ts
@@ -1,10 +1,28 @@
 import {AbstractParser} from "./AbstractParser";
 import Tokenizer from "../tokenizer/Tokenizer";
-import {SectionText} from "../types/Text";
+import {SectionText, Text} from "../types/Text";
+import Tokens from "../tokenizer/Tokens"
+import { TextParser } from "./TextParser";
 
 export class SummaryParser extends AbstractParser {
     parse(context: Tokenizer): SectionText {
-        // TODO implement
+        // TODO
+        let sectionText = {} as SectionText
+        let value = [] as Text[]
+
+        // first check if its bullet pointed entry
+        let nextToken = context.peek()
+        if(nextToken === Tokens.BULLET_KEY){
+            sectionText.is_bullet = true
+            context.getNext()   // consume the bullet token
+        } else {
+            sectionText.is_bullet = false
+        }
+
+        // if not null and not NEW_LINE then there is a text to parse in same line
+        while (nextToken !== null && nextToken !== Tokens.NEW_LINE) {
+            value.push(new TextParser().parse(context))
+        }
         return {} as SectionText
     }
 }
