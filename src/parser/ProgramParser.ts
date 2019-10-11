56d6ce41af59211847bb0674d40b0ce192233a90
egeokt <egeokten@gmail.com>
2019-10-10 20:24:10 -0700

    Add lines to execute body parsing

--- src/parser/ProgramParser.ts
+++ src/parser/ProgramParser.ts
@@ -16,11 +16,11 @@ export class ProgramParser extends AbstractParser {
                 `at line: ${context.getCurrentLine()}`)
         }
 
-        // let body = new BodyParser()
-        // body.parse(context)
+        let bodyParser = new BodyParser()
+        let parsedBody = bodyParser.parse(context)
         return {
             header: header,
-            body: {} as Body //TODO
+            body: parsedBody
         }
     }
 }
